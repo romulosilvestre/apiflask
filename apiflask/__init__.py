9886d2cd5eaaa608b8d3b95632e9608b518191cc
romulosilvestre <romulo.cesardf@hotmail.com>
2024-04-09 19:17:50 -0300

    aula base

---
+++ apiflask/__init__.py
@@ -0,0 +1,24 @@
+#importando o framework Flask
+from flask import Flask
+#importando o framework SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+
+#definindo a aplicação
+app = Flask(__name__)
+#definindo um banco de dados
+db = SQLAlchemy(app)
+
+
+#adicionando o arquivo config.py
+#app.config.from_object('config')
+
+#definindo a rota com uma função def
+@app.route("/")
+def index():
+    return "Olá Mundo!"
+
+#executando a aplicação
+if __name__ == "__main__":
+    app.run()
+
